8bb2bba709028cccae922b83980537140c8ef3be
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-11-30 19:03:33 +0000

    Initial commit

--- frontend/src/components/PageHeader.tsx
+++
@@ -1,3 +0,0 @@
-export function PageHeader(): JSX.Element {
-  return <h1>Welcome to my To-do App!</h1>;
-}
--- frontend/src/components/ToDoList.tsx
+++
@@ -1,3 +0,0 @@
-export function ToDoList(): JSX.Element {
-  return <h1>Main Content</h1>;
-}
